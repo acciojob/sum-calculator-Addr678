194c8fecb9a49a86e94815ef613262ed0fbc77ec
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-07-23 12:10:09 +0000

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from 'react';
 
-const App = () => {
+const SumCalculator = () => {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  useEffect(() => {
+    let isMounted = true;
+    const calculateSum = async () => {
+      // Simulate asynchronous calculation to prevent UI freezes
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      if (isMounted) {
+        const totalSum = numbers.reduce((acc, num) => acc + num, 0);
+        setSum(totalSum);
+      }
+    };
+
+    calculateSum();
+
+    return () => {
+      isMounted = false;
+    };
+  }, [numbers]);
+
+  const handleNumberChange = (event) => {
+    const newNumbers = event.target.value
+      .split(',')
+      .map((num) => parseFloat(num.trim()))
+      .filter((num) => !isNaN(num));
+
+    setNumbers(newNumbers);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Sum Calculator</h2>
+      <div>
+        <label htmlFor="numbersInput">Enter numbers separated by commas:</label>
+        <input
+          type="text"
+          id="numbersInput"
+          onChange={handleNumberChange}
+          placeholder="e.g., 1, 2, 3"
+        />
+      </div>
+      <div>
+        <p>Sum: {sum}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default SumCalculator;
